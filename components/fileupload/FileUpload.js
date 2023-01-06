618892e545cb0983d3c4e5c64599cb5e5cced466
Jacob Sussman <116610914+Jacob-Sussman@users.noreply.github.com>
2023-01-05 22:12:52 -0600

    Adding FileUpload functionality

--- components/fileupload/FileUpload.js
+++ components/fileupload/FileUpload.js
@@ -1,6 +1,20 @@
 import { Card, Text, Link } from "@nextui-org/react";
 
 export default function FileUpload(props) {
+  const handleDrop = (event) => {
+    event.preventDefault();
+    const file =
+      event.dataTransfer !== undefined
+        ? event.dataTransfer.files[0]
+        : event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = () => {
+      // reader.result contains the text of the file
+      console.log(reader.result);
+    };
+    reader.readAsText(file);
+  };
+
   return (
     <Card css={{ mw: "450px", height: "200px" }}>
       <Card.Body>
@@ -14,13 +28,16 @@ export default function FileUpload(props) {
             borderRadius: "10px",
             border: "1px dashed #ccc",
           }}
+          onDrop={handleDrop}
+          onDragOver={(e) => e.preventDefault()}
         >
           <input
             type="file"
             id="file-input"
             //   ref={#}
-            //   onChange={#}
-            accept="image/*"
+            onChange={handleDrop}
+            // accept=".txt/.pdf"
+            accept=".docx, .pdf, .txt"
             // style={{ display: "none" }}
             style={{
               position: "absolute",
