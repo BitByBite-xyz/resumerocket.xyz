840c9ca6933239c82df0f30f599f949aa47c7637
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-01-17 23:24:18 -0800

    yum

--- pages/index.js
+++ pages/index.js
@@ -29,33 +29,9 @@ export const AcmeLogo = () => (
 export default function Index() {
   const router = useRouter();
   return (
-    <Layout>
+    <>
        
-       <Navbar isBordered variant="floating">
-        <Navbar.Brand>
-          <AcmeLogo />
-          <Text b color="inherit" hideIn="xs">
-            ACME
-          </Text>
-        </Navbar.Brand>
-        <Navbar.Content hideIn="xs">
-          <Navbar.Link href="#">Features</Navbar.Link>
-          <Navbar.Link isActive href="#">Customers</Navbar.Link>
-          <Navbar.Link href="#">Pricing</Navbar.Link>
-          <Navbar.Link href="#">Company</Navbar.Link>
-        </Navbar.Content>
-        <Navbar.Content>
-          <Navbar.Link color="inherit" href="#">
-            Login
-          </Navbar.Link>
-          <Navbar.Item>
-            <Button auto flat as={Link} href="#">
-              Sign Up
-            </Button>
-          </Navbar.Item>
-        </Navbar.Content>
-      </Navbar>
-
+       <Header/>
       <main className={styles.main}>
         {/* <div className={styles.description}>
           <p>
@@ -224,6 +200,6 @@ export default function Index() {
           </Grid>
         </Grid.Container>
       </main>
-    </Layout>
+    </>
   );
 }
