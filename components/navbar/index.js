47cfbbf8b6370586b67a73a595f9ae3aa885ae06
Jacob Sussman <116610914+Jacob-Sussman@users.noreply.github.com>
2023-01-18 14:39:08 -0600

    Navbar mobile responsive
    -Added mobile Navbar

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -1,49 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { Navbar, Button, Link, Text } from "@nextui-org/react";
 import { AcmeLogo } from "./Logo.js";
-import { useTheme } from "next-themes";
 import { useRouter } from "next/router";
 
 export default function CustomNavBar() {
   const router = useRouter();
   const { pathname } = router;
-  const { theme, setTheme } = useTheme();
 
   return (
     <Navbar isCompact isBordered variant="sticky">
       <Navbar.Brand onClick={() => router.push("/")}>
+        <Navbar.Toggle showIn="xs" aria-label="toggle navigation" />
         <AcmeLogo />
         <Text b color="inherit" hideIn="xs">
           Resume Rocket
         </Text>
       </Navbar.Brand>
-      <Navbar.Content
-        hideIn="xs"
-        variant="underline"
-
-        // onAction={(actionKey) => {
-
-        //   switch (actionKey) {
-        //     case 'pricing]':
-        //       router.push('/pricing]');
-        //       break;
-        //     case 'examples':
-        //       //setTheme(theme === 'light' ? 'dark' : 'light')
-        //       router.push('/examples]');
-
-        //       break;
-        //     case 'help_and_feedback':
-        //       console.log('Mangoes and papayas are $2.79 a pound.');
-        //       alert('coming soon\n\n\n\t\t\t\t\t\t:)')
-        //       // expected output: "Mangoes and papayas are $2.79 a pound."
-        //       break;
-        //     default:
-        //       console.log(`Sorry, we are out of.`);
-        //   }
-
-        //   console.log({ actionKey })
-        // }}
-      >
+      <Navbar.Content hideIn="xs" variant="underline">
         <Navbar.Link
           onClick={() => {
             router.push("/home");
@@ -81,6 +54,47 @@ export default function CustomNavBar() {
           </Button>
         </Navbar.Item>
       </Navbar.Content>
+      <Navbar.Collapse disableAnimation>
+        <Navbar.CollapseItem
+          activeColor="warning"
+          // css={{
+          //   color: index === collapseItems.length - 1 ? "$error" : "",
+          // }}
+          // isActive={index === 2}
+        >
+          <Link
+            color="inherit"
+            css={{
+              minWidth: "100%",
+            }}
+            onClick={() => router.push("/home")}
+          >
+            Create a Cover Letter
+          </Link>
+        </Navbar.CollapseItem>
+        <Navbar.CollapseItem activeColor="warning">
+          <Link
+            color="inherit"
+            css={{
+              minWidth: "100%",
+            }}
+            onClick={() => router.push("/pricing")}
+          >
+            Pricing
+          </Link>
+        </Navbar.CollapseItem>
+        <Navbar.CollapseItem activeColor="warning">
+          <Link
+            color="inherit"
+            css={{
+              minWidth: "100%",
+            }}
+            onClick={() => router.push("/login")}
+          >
+            Examples
+          </Link>
+        </Navbar.CollapseItem>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
