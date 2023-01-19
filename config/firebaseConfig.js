492b13ed84f1c00bade054f2ce789132b9f4627f
Jacob Sussman <116610914+Jacob-Sussman@users.noreply.github.com>
2023-01-18 22:58:16 -0600

    Firebase config
    -Added Firebase

---
+++ config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBf3BhZKvqIaUwOHRnqgm89qUbuoklcLrM",
+  authDomain: "bitbybite-dotxyz.firebaseapp.com",
+  projectId: "bitbybite-dotxyz",
+  storageBucket: "bitbybite-dotxyz.appspot.com",
+  messagingSenderId: "338156763075",
+  appId: "1:338156763075:web:244b7e02af1c899b5cef76",
+  measurementId: "G-1E858JGC2R",
+};
