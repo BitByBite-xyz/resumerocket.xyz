b7b013a186d1d4d3b3751cb8bcd38f0f49242940
Jacob Sussman <116610914+Jacob-Sussman@users.noreply.github.com>
2023-01-18 23:04:56 -0600

    Fixed Firebase config

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -4,6 +4,6 @@ export const firebaseConfig = {
   projectId: "bitbybite-dotxyz",
   storageBucket: "bitbybite-dotxyz.appspot.com",
   messagingSenderId: "338156763075",
-  appId: "1:338156763075:web:244b7e02af1c899b5cef76",
-  measurementId: "G-1E858JGC2R",
+  appId: "1:338156763075:web:ac90aa05e780dff75cef76",
+  measurementId: "G-2CBQ7H8ZNC",
 };
