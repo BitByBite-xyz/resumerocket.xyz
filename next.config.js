6ebceae8cf0e536e4c96c5470eb8a0fd032e13a6
connlark <connor@bitbybite.xyz>
2023-02-06 14:01:49 -0800

    🌊

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    runtime: 'edge'    
+    
+  },
+  
 }
+
 
 module.exports = nextConfig
