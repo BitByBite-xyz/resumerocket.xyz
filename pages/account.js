a0bff8b3bb71be45fb4a7f60bb616bcbaf1d15f8
Jacob Sussman <116610914+Jacob-Sussman@users.noreply.github.com>
2023-02-11 11:28:16 -0600

    Added user email to account page
    - User's email is now displayed under account section

--- pages/account.js
+++ pages/account.js
@@ -1,7 +1,30 @@
-import { Button, Input, Grid, Text, Row } from "@nextui-org/react";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { Input, Grid, Text, Row } from "@nextui-org/react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import SettingNav from "../components/settings/SettingNav";
 
 export default function Account() {
+  const [user, setUser] = useState({});
+
+  const auth = getAuth();
+  const router = useRouter();
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in
+      if (user.email !== "") {
+        setUser({
+          email: user.email,
+          uid: user.uid,
+        });
+      }
+    } else {
+      // If user is not logged in, redirect to login page
+      router.push("/login");
+    }
+  });
+
   return (
     <Grid.Container>
       <Grid md={2} xs={4}>
@@ -17,7 +40,7 @@ export default function Account() {
               <Input
                 bordered
                 label="Email"
-                initialValue="jacob@bitbybite.xyz"
+                initialValue={user.email}
                 color="primary"
                 width="100%"
                 size="md"
