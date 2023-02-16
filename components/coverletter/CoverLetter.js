c71ae9a6062f106ae772b7c543281b24a408d967
Jacob Sussman <116610914+Jacob-Sussman@users.noreply.github.com>
2023-02-16 12:32:24 -0600

    Added line breaks to cover letters
    - Cover letters generated now contain line breaks
    - Styling fixes done on CoverLetter component padding

--- components/coverletter/CoverLetter.js
+++ components/coverletter/CoverLetter.js
@@ -4,12 +4,19 @@ import { DownloadIcon } from "./DownloadIcon";
 
 export default function CoverLetter(props) {
   return (
-    <Card style={{ padding: "20px 40px 20px 40px" }} css={{ width: 800 }}>
+    <Card style={{ padding: "0px 40px 20px 40px" }} css={{ width: 800 }}>
       <IconButton style={{ position: "absolute", marginLeft: 540 }}>
         {props.isDownloadAble ? <DownloadIcon size={20} /> : null}
       </IconButton>
       <Card.Body css={{ py: "$10" }}>
-        <Text>{props.text}</Text>
+        <Text>
+          {props.text.split("\n").map((line, index) => (
+            <span key={index}>
+              {line}
+              <br />
+            </span>
+          ))}
+        </Text>
       </Card.Body>
     </Card>
   );
--- pages/home.js
+++ pages/home.js
@@ -98,7 +98,7 @@ export default function Home() {
             formRef={fileInput}
           />
         </Grid>
-        <Grid xs={6}>{text ? <CoverLetter text={text} /> : null}</Grid>
+        <Grid xs={7}>{text ? <CoverLetter text={text} /> : null}</Grid>
         <Grid style={{ position: "absolute", marginTop: 250 }} md={6} xs={9.5}>
           <Card
             style={{ paddingLeft: 23, paddingTop: 15, paddingBottom: 15 }}
