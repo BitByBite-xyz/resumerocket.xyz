3f5fdb8186ee52832bdb5a05627e25aa6de90ffa
Jacob Sussman <116610914+Jacob-Sussman@users.noreply.github.com>
2023-03-06 16:35:42 -0600

    Fixed downloaded cover letter format

--- components/coverletter/CoverLetter.js
+++ components/coverletter/CoverLetter.js
@@ -5,33 +5,37 @@ import { IconButton } from "../fileupload/IconButton";
 import { DownloadIcon } from "./DownloadIcon";
 
 export default function CoverLetter(props) {
-  const downloadCoverLetter = () => {
-    const doc = new jsPDF({
-      orientation: "portrait",
-      unit: "mm",
-      format: "letter",
-      margins: {
-        top: 25.4, // 1 inch
-        bottom: 25.4,
-        left: 25.4,
-        right: 25.4,
-      },
+  function createPDF(text) {
+    // Create a new jsPDF instance
+    const doc = new jsPDF({ format: "letter", unit: "mm" });
+
+    // Set the font size and type
+    doc.setFontSize(12);
+    doc.setFont("times", "roman");
+
+    // Set the maximum width of the text
+    const maxWidth = doc.internal.pageSize.getWidth() - 40; // Subtract 20 to account for margins
+
+    // Split the text into an array of lines based on the maximum width
+    const lines = doc.splitTextToSize(props.text, maxWidth);
+
+    // Add each line of text to the PDF document
+    let y = 20; // Start at 20mm from the top
+    lines.forEach((line) => {
+      doc.text(line, 20, y);
+      y += 6; // Add 5mm between each line
     });
 
-    doc.setFont("times", "roman");
-    doc.setFontSize(12);
-
-    doc.text(props.text, 25.4, 25.4 + 12); // account for 1 inch margin and font size
-
+    // Save the PDF document
     doc.save(
       `${props.company.toLocaleLowerCase()}-${props.jobTitle.toLocaleLowerCase()}-cover-letter.pdf`
     );
-  };
+  }
 
   return (
     <Card style={{ padding: "20px 40px 20px 40px" }} css={{ width: 800 }}>
       <IconButton style={{ marginLeft: "93%", marginTop: 10 }}>
-        <DownloadIcon onClick={downloadCoverLetter} size={20} fill="#FF0080" />
+        <DownloadIcon onClick={createPDF} size={20} fill="#FF0080" />
       </IconButton>
       <Card.Body css={{ py: "$10" }}>
         <Text>
